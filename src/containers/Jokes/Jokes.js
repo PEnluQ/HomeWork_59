c7a5784c103a8e5032c6248d46f911f8c9c0edeb
dell <khasyu@mail.ru>
2019-12-05 16:13:14 +0600

    Created components

---
+++ src/containers/Jokes/Jokes.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Jokes extends Component {
+    render() {
+        return (
+            <div>
+            Jokes
+            </div>
+        );
+    }
+}
+
+export default Jokes;
